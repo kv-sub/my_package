0dcd7fbe7aacc67b5b551089e175ad36ff0a6f6b
kv <kvsubramanian03@gmail.com>
2024-03-24 14:56:49 +0530

    first commit

---
+++ launch/listener.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_Launch_description():
+    return LaunchDescription([
+        Node(
+            package='demo_nodes_py',
+            executable='listener'
+        )
+    ])
